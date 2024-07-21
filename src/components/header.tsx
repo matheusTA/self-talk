0e423f96be38ede409352f3981d183481641268f
matheusTA <matrajano12@gmail.com>
2024-07-21 17:52:35 -0300

    refactor: change href link to settings

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
         <ThemeSwitcher />
 
         <Button asChild variant="ghost" size="icon" className="group">
-          <Link href="/auth/sign-in">
+          <Link href="/settings">
             <GearIcon className="size-5 group-hover:animate-spin-halfway" />
           </Link>
         </Button>
