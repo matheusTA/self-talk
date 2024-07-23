a50f1ea431581cee1cb7a5887d16ee48a342d74f
matheusTA <matrajano12@gmail.com>
2024-07-23 02:16:05 -0300

    feat: create a initial structure to home page form

--- src/app/(home)/create-chat-form.tsx
+++ src/app/(home)/create-chat-form.tsx
@@ -1,7 +1,126 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { DoubleArrowRightIcon } from "@radix-ui/react-icons";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const formSchema = z.object({
+  topic: z.string({ required_error: "Please enter a topic" }).min(1),
+  lang: z.string({ required_error: "Please select a language" }).min(1),
+  duration: z.string({ required_error: "Please select a duration" }).min(1),
+});
+
 export default function CreateChatForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      topic: "",
+      lang: "",
+      duration: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data);
+  }
+
   return (
-    <form action="">
-      <h1>FORM</h1>
-    </form>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-5"
+      >
+        <FormField
+          control={form.control}
+          name="topic"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>What topic do you want to talk about?</FormLabel>
+              <FormControl>
+                <Input placeholder="Travel" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex flex-col gap-4 lg:flex-row">
+          <FormField
+            control={form.control}
+            name="lang"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>In which language will we speak?</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a language" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="pt-br">Portugues - Brasil</SelectItem>
+                    <SelectItem value="en-us">English - USA</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="duration"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>How long will the conversation last?</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a duration" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="SHORT">Short</SelectItem>
+                    <SelectItem value="MEDIUM">Medium</SelectItem>
+                    <SelectItem value="LONG">Long</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <Button type="submit" className="self-end">
+          Start
+          <DoubleArrowRightIcon className="ml-2 size-4" />
+        </Button>
+      </form>
+    </Form>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,7 +2,7 @@ import CreateChatForm from "./create-chat-form";
 
 export default function HomePage() {
   return (
-    <div className="flex size-full flex-col items-center justify-center gap-10">
+    <div className="flex size-full flex-col items-center justify-center gap-20">
       <h1 className="inline-block bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-center text-4xl font-bold text-transparent">
         The{" "}
         <span className="underline decoration-black decoration-wavy decoration-1 dark:decoration-white">
