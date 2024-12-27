79efd6c3f91ccfb726612a064c9311068d14be3f
matheusTA <matrajano12@gmail.com>
2024-12-27 01:00:14 -0300

    fix: remove unnecessary span for improved heading clarity

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -15,10 +15,7 @@ export default function HomePage() {
     <div className="flex size-full flex-col items-center justify-center gap-20">
       <div className="flex flex-col items-center gap-4">
         <h1 className="inline-block bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-center text-[48px] font-bold text-transparent">
-          Unlock your conversation skills with{" "}
-          <span className="underline decoration-black decoration-wavy decoration-1 dark:decoration-white">
-            AI voice chat
-          </span>
+          Unlock your conversation skills with AI voice chat
         </h1>
 
         <span className="text-center text-lg font-medium text-muted-foreground">
